57e10dfca8eb1a108ed3308ad3a52d0cf8f018cf
George Vasiliadis <gvasiliadis72@gmail.com>
2025-07-08 21:08:07 +1000

    Python file removed. SQL file created.

---
+++ cinema.sql
@@ -0,0 +1,7 @@
+-- Create Database in Postresql in terminal first
+-- The rest can be done in the IDE(Written here)
+-- Command to run in the terminal to run the sql file 'psql -U *username*(i.e. postgres or specific user) -d *database name* -f *filename*.sql'
+-- In this example it will be 'psql -U postgres -d cinema_db -f cinema.sql'
+-- Make sure to CREATE TABLE from least dependent to most dependent, and in reverse when DROP TABLE IF EXIST
+
+
